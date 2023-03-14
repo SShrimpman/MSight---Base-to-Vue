0cbb0fb9021c3d365a459e02c8fc53dd374af1e5
António Camarão <camaraoacsn@gmail.com>
2023-03-14 10:35:46 +0000

    Context Menu works perfectly!

--- src/components/contextMenu/ContextMenu.vue
+++ src/components/contextMenu/ContextMenu.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script>
-import { setCameraLookingPoint, setCameraLookingWorldCenter } from "../../helpers/camera";
-
     export default{
         // props: {
         //     mousePos: {
